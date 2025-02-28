51435e860c4d664f87f7c85835045e5aafa132fe
runedrummer81 <runedrummer81@gmail.com>
2025-02-28 12:07:00 +0100

    Filter-knap virker

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 "use strict"
 
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".dropbtn");
+    const dropdown = document.querySelector(".dropdown-content");
+
+    button.addEventListener("click", function(event) {
+        event.stopPropagation();
+        dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    });
+
+    document.addEventListener("click", function() {
+        dropdown.style.display = "none";
+    });
+});
