0484a9eb5afb3fa662d3aa38b30ec6a7b20c95a7
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-03-05 11:55:01 +0100

    Produkter

--- js/shop.js
+++ js/shop.js
@@ -8,84 +8,106 @@ let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
-openShopping.addEventListener('click', ()=>{
+openShopping.addEventListener('click', () => {
     body.classList.add('active');
-})
-closeShopping.addEventListener('click', ()=>{
+});
+closeShopping.addEventListener('click', () => {
     body.classList.remove('active');
-})
+});
 
 let products = [
     {
         id: 1,
         name: 'Pilsner',
         image: 'img/pilsner.webp',
-        price: 22
+        price: 22,
+        dateAdded: '2023-01-15',
     },
     {
         id: 2,
         name: 'Spiky Ginger',
         image: 'img/spiky-ginger.webp',
-        price: 25
+        price: 25,
+        dateAdded: '2023-02-15',
     },
     {
         id: 3,
         name: 'Pinky Promise',
         image: 'img/pinky-promise.webp',
-        price: 25
+        price: 25,
+        dateAdded: '2023-04-15',
     },
     {
         id: 4,
         name: 'Basil Smash',
         image: 'img/basil-smash.webp',
-        price: 25
+        price: 25,
+        dateAdded: '2023-03-15',
     },
     {
         id: 5,
         name: 'Beetylychious',
         image: 'img/beetylychouis.webp',
-        price: 28
+        price: 28,
+        dateAdded: '2025-01-15',
     },
     {
         id: 6,
         name: 'Berry Bomb',
         image: 'img/berry-bomb.webp',
-        price: 28
+        price: 28,
+        dateAdded: '2024-01-15',
     }
 ];
-let listCards  = [];
-function initApp(){
-    products.forEach((value, key) =>{
+
+let listCards = [];
+
+function initApp() {
+    products.forEach((value, key) => {
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
+        // Extract the image name without the extension for the link
+        let imageName = value.image.split('/').pop().split('.')[0];
+
         newDiv.innerHTML = `
-            <img src="${value.image}">
+            <a href="produkter/${imageName}.html">
+                <img src="${value.image}" alt="${value.name}">
+            </a>
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
             <button onclick="addToCard(${key})">Add To Card</button>`;
         list.appendChild(newDiv);
-    })
+    });
 }
+
 initApp();
-function addToCard(key){
-    if(listCards[key] == null){
-        // copy product form list to list card
+
+function addToCard(key) {
+    if (listCards[key] == null) {
+        // Copy product from list to list card
         listCards[key] = JSON.parse(JSON.stringify(products[key]));
         listCards[key].quantity = 1;
     }
     reloadCard();
 }
-function reloadCard(){
+
+function reloadCard() {
     listCard.innerHTML = '';
     let count = 0;
     let totalPrice = 0;
-    listCards.forEach((value, key)=>{
+    listCards.forEach((value, key) => {
         totalPrice = totalPrice + value.price;
         count = count + value.quantity;
-        if(value != null){
+        if (value != null) {
             let newDiv = document.createElement('li');
+
+            // Extract the image name without the extension for the link
+            let imageName = value.image.split('/').pop().split('.')[0];
+
             newDiv.innerHTML = `
-                <div><img src="${value.image}"/></div>
+                <a href="produkter/${imageName}.html">
+                    <img src="${value.image}" alt="${value.name}">
+                </a>
                 <div>${value.name}</div>
                 <div>${value.price.toLocaleString()}</div>
                 <div>
@@ -93,16 +115,17 @@ function reloadCard(){
                     <div class="count">${value.quantity}</div>
                     <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
                 </div>`;
-                listCard.appendChild(newDiv);
+            listCard.appendChild(newDiv);
         }
-    })
+    });
     total.innerText = totalPrice.toLocaleString();
     quantity.innerText = count;
 }
-function changeQuantity(key, quantity){
-    if(quantity == 0){
+
+function changeQuantity(key, quantity) {
+    if (quantity == 0) {
         delete listCards[key];
-    }else{
+    } else {
         listCards[key].quantity = quantity;
         listCards[key].price = quantity * products[key].price;
     }
