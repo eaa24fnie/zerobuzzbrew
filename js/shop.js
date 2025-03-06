5e64cbf6f3543e7916ef4198d3860bfa4c5c3405
MalouStoustrup <malou.stoustrup@gmail.com>
2025-03-06 09:24:35 +0100

    padding på kruven

--- js/shop.js
+++ js/shop.js
@@ -20,7 +20,6 @@ window.onload = function () {
 }; 
 
 
-
 let openShopping = document.querySelector('.shopping');
 let closeShopping = document.querySelector('.closeShopping');
 let list = document.querySelector('.list');
